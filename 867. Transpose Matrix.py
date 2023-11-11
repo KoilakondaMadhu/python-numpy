418aec0349660a368616eddc05397d6be1966436
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-11 20:34:22 +0530

    Update 867. Transpose Matrix.py

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -14,12 +14,12 @@ class Solution:
         # Return the transposed list
         return transposed_list
 
-iven a 2D integer array matrix, return the transpose of matrix.
+# given a 2D integer array matrix, return the transpose of matrix.
 
-The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+# The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
 
 
-
+# https://leetcode.com/problems/design-graph-with-shortest-path-calculator/description/?envType=daily-question&envId=2023-11-11
 
 # Case 1
 # Case 2
