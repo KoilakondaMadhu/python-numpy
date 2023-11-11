5a23ee75c8182e050a5fe9e9719455a9ca6442ed
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-11 20:26:00 +0530

    Create 867. Transpose Matrix.py

---
+++ 867. Transpose Matrix.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class Solution:
+    def transpose(self, A):
+        # Convert the input matrix A to a NumPy array
+        nparr = np.array(A)
+        
+        # Transpose the NumPy array
+        nparr = np.transpose(nparr)
+        
+        # Convert the transposed NumPy array back to a list
+        transposed_list = nparr.tolist()
+        
+        # Return the transposed list
+        return transposed_list
+
+iven a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+
+
+
+# Case 1
+# Case 2
+# Input
+# matrix =
+# [[1,2,3],[4,5,6],[7,8,9]]
+# Output
+# [[1,4,7],[2,5,8],[3,6,9]]
+# Expected
+# [[1,4,7],[2,5,8],[3,6,9]]
+
+
+# ===============================
+ 
+
+# Example 1:
+
+# Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+# Output: [[1,4,7],[2,5,8],[3,6,9]]
+# Example 2:
+
+# Input: matrix = [[1,2,3],[4,5,6]]
+# Output: [[1,4],[2,5],[3,6]]
+ 
