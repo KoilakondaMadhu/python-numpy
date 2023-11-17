606ec1bbf0cfd08f7b5759cf36deb8005955719e
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-17 22:30:55 +0530

    Create create full numpyarray.py

---
+++ create full numpyarray.py
@@ -0,0 +1,14 @@
+import numpy as np  # Import the NumPy library and use the alias np for convenience
+
+# Create a 2D array (matrix) with dimensions 3x4, filled with the value 5
+full_array2d = np.full((3, 4), 5)
+
+# Print the resulting 2D array
+print(full_array2d)
+
+
+# Output
+
+# [[5 5 5 5]
+#  [5 5 5 5]
+#  [5 5 5 5]]
