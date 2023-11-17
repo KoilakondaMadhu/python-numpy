9930f21316bc50472e0d1c296c8acaa09bedbea6
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-17 22:25:37 +0530

    Create How to create an empty and a full NumPy array?.py

---
+++ How to create an empty and a full NumPy array?.py
@@ -0,0 +1,16 @@
+import numpy as np  # Import the NumPy library and alias it as 'np'
+
+# Create an empty 2D array with shape (3, 4)
+empty_array_2d = np.empty((3, 4))
+
+# Print the contents of the empty 2D array
+print(empty_array_2d)
+
+
+# output
+# [[6.57685576e-310 0.00000000e+000 0.00000000e+000 0.00000000e+000]
+#  [0.00000000e+000 0.00000000e+000 0.00000000e+000 0.00000000e+000]
+#  [0.00000000e+000 0.00000000e+000 0.00000000e+000 0.00000000e+000]]
+
+
+# https://colab.research.google.com/drive/19T0_HoBU0psX3-6wvo4EAcDoNIl6NSJy#scrollTo=rQmFrYHZNJfb
